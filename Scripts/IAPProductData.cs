dfc4245d4d521a748d2033eaf5f44084dbcecf9a
nguyentrong101094 <nguyentrong101094@gmail.com>
2021-03-29 17:18:48 +0700

    add default price to product data to display when does not have internet connection

--- Scripts/IAPProductData.cs
+++ Scripts/IAPProductData.cs
@@ -6,10 +6,32 @@ using UnityEngine.Purchasing;
 [CreateAssetMenu(fileName = "ProductData", menuName = "IAP Product Data")]
 public class IAPProductData : ScriptableObject
 {
-
     public string ProductId { get => name; }
     public ProductType productType = ProductType.Consumable;
+    [Tooltip("Price to display in dollar when getting product info from server failed")]
+    public float defaultPrice;
     public Payout[] payouts = new Payout[] { new Payout() };
+
+    Product GetProduct()
+    {
+        return InAppPurchaseHelper.instance.GetProduct(ProductId);
+    }
+
+    public string LocalizedPriceString
+    {
+        get
+        {
+            var product = GetProduct();
+            if (product != null)
+            {
+                return product.metadata.localizedPriceString;
+            }
+            else
+            {
+                return $"${defaultPrice}";
+            }
+        }
+    }
 }
 
 [System.Serializable]
