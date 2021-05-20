08441766d1459d94124490677dcf4ff169886b1f
nguyentrong101094 <nguyentrong101094@gmail.com>
2021-05-20 10:28:18 +0700

    add comment header to payout type

--- Scripts/IAPProductData.cs
+++ Scripts/IAPProductData.cs
@@ -6,7 +6,6 @@ using UnityEngine.Purchasing;
 [CreateAssetMenu(fileName = "ProductData", menuName = "IAP Product Data")]
 public class IAPProductData : ScriptableObject
 {
-
     public string ProductId { get => name; }
     public ProductType productType = ProductType.Consumable;
     public Payout[] payouts = new Payout[] { new Payout() };
@@ -16,5 +15,9 @@ public class IAPProductData : ScriptableObject
 public class Payout
 {
     public int quantity;
+
+    [Header("Type: Currency = 1, Item = 2, Other = 3, NoAds = 4")]
     public PayoutType.Type type;
+
+    public int subtype;
 }
