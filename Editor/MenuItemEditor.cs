621bee0f38ac082eac23fe26a13148f038e1ddea
nguyentrong101094 <nguyentrong101094@gmail.com>
2022-04-06 17:49:16 +0700

    upgrade product data type & subtype no longer automatic via OnValidate, will need to be called manually from menu item.

---
+++ Editor/MenuItemEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Omnilatent.InAppPurchase.Editor
+{
+    public static class MenuItemEditor
+    {
+        [MenuItem("Tools/Omnilatent/IAP Helper/Upgrade ProductData v1 to v2")]
+        public static void UpgradeProductDataTypeSubtype()
+        {
+            var productDatas = Resources.LoadAll<IAPProductData>(IAPProcessor.dataFolder);
+            for (int i = 0; i < productDatas.Length; i++)
+            {
+                productDatas[i].UpgradeTypeSubtype();
+            }
+        }
+    }
+}
--- Scripts/IAPProductData.cs
+++ Scripts/IAPProductData.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Unity.Collections;
 using UnityEngine;
 using UnityEngine.Purchasing;
+using UnityEngine.Serialization;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -58,26 +59,27 @@ public class IAPProductData : ScriptableObject
     }
 
 #if UNITY_EDITOR
-    private void OnValidate()
+    public void UpgradeTypeSubtype()
     {
+        bool upgrading = false;
         for (int i = 0; i < payouts.Length; i++)
         {
-            if (payouts[i].payoutType == PayoutTypeEnum.NotSet && payouts[i].type != 0)
+            upgrading |= payouts[i].CheckUpdatePayoutType(name, out bool upgradeType, out bool upgradeSubtype);
+            if (upgradeType)
             {
-                if (Enum.IsDefined(typeof(PayoutTypeEnum), (int)payouts[i].type))
-                {
-                    Debug.Log($"Detected deprecated [type] value in {name}. [payoutType] will be updated to match [type] value.");
-                    payouts[i].payoutType = (PayoutTypeEnum)(int)payouts[i].type;
-
-                    EditorUtility.SetDirty(this);
-                    AssetDatabase.SaveAssets();
-                    AssetDatabase.Refresh();
-                }
-                else
-                {
-                    Debug.LogError($"Detected deprecated [type] value in {name}. Unable to match [payoutType] to [type] value. Please set [payoutType] to 'Other' and use [subtype] instead");
-                }
+                Debug.Log($"Detected deprecated [type] value in {name}. [payoutType] will be updated to match [type] value.");
             }
+            if (upgradeSubtype)
+            {
+                Debug.Log($"Detected deprecated [subtype] value in {name}. [subtypeId] will be updated to match [subtype] value.");
+            }
+        }
+
+        if (upgrading)
+        {
+            EditorUtility.SetDirty(this);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
         }
     }
 #endif
@@ -88,9 +90,49 @@ public class Payout
 {
     public int quantity;
     public PayoutTypeEnum payoutType;
-    public string subtype;
+    [SerializeField] string subtypeId;
+
+    [Header("Deprecated fields, do not use.")]
 
     [System.Obsolete("Use payoutType instead")]
     [Tooltip("PayoutType.Type is deprecated. Do not use this field, use payoutType field instead.")]
     [ReadOnly] public PayoutType.Type type;
+
+    [System.Obsolete("Use subtypeId instead")]
+    [FormerlySerializedAs("subtype")]
+    [SerializeField] int _subtypeInt = 0;
+
+    public string subtype { get => subtypeId; }
+
+    /// <summary>
+    /// Update payout type & subtype when upgrading from Omni IAP 1.1.1 to 2.0.0
+    /// </summary>
+    /// <returns>True if fields were updated</returns>
+    public bool CheckUpdatePayoutType(object invokerName, out bool upgradeType, out bool upgradeSubtype)
+    {
+        upgradeType = false;
+        upgradeSubtype = false;
+        if (payoutType == PayoutTypeEnum.NotSet && type != 0)
+        {
+            if (Enum.IsDefined(typeof(PayoutTypeEnum), (int)type))
+            {
+                if (payoutType != (PayoutTypeEnum)(int)type)
+                {
+                    payoutType = (PayoutTypeEnum)(int)type;
+                    upgradeType = true;
+                }
+            }
+            else
+            {
+                Debug.LogError($"Detected deprecated [type] value '{type}' in {invokerName}. Unable to match [payoutType] to [type] value. Please set [payoutType] to 'Other' and use [subtype] instead");
+            }
+        }
+
+        if (_subtypeInt != 0 && subtypeId != _subtypeInt.ToString())
+        {
+            subtypeId = _subtypeInt.ToString();
+            upgradeSubtype = true;
+        }
+        return upgradeType || upgradeSubtype;
+    }
 }
